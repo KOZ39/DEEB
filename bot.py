78350171641782093e89382690e866d31b83877a
KOZ39 <koz39c@gmail.com>
2023-05-30 03:17:25 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,16 +5,16 @@ import discord
 
 SINGLE_EMOJI_REGEX = re.compile(
     r"""
-    ^          # Start of string
-    (?!<.*<)   # Negative lookahead to ensure there is not more than one '<' at the beginning
-    <          # Emoji opening delimiter
-    (a)?       # Optional 'a' for animated emojis
-    :          # Colon delimiter
-    (.+?)      # Emoji name
-    :          # Colon delimiter
+    ^               # Start of string
+    (?!<.*<)        # Negative lookahead to ensure there is not more than one '<' at the beginning
+    <               # Emoji opening delimiter
+    (a)?            # Optional 'a' for animated emojis
+    :               # Colon delimiter
+    (.+?)           # Emoji name
+    :               # Colon delimiter
     ([0-9]{15,21})  # Emoji ID
-    >          # Emoji closing delimiter
-    $          # End of string
+    >               # Emoji closing delimiter
+    $               # End of string
     """,
     re.VERBOSE,
 )
